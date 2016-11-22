d586a51f1fd3d9a48616341a924d56140a963836
Christopher Hodges <chris@vabeachwebdeveloper.com>
2016-11-22 09:52:10 -0500

    qa fixes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <section class="hero">
 	<div class="container">
 		<div class="hero__inner">
-			<h2 class="hero__quote"><span class="open-quote">&ldquo;</span>Help me help my fur-friends.<span class="close-quote">&rdquo;</span></h2>
+			<h2 class="hero__quote"><span class="open-quote">&ldquo;</span><div>Help me help my fur-friends.</div><span class="close-quote">&rdquo;</span></h2>
 			<span class="hero__motto">-GP, the self-appointed liaison between pets & people.</span>
 			<ul class="hero__dontateButtons">
 				<li><a target="_blank" href="https://interland3.donorperfect.net/weblink/FormSingleNM.aspx?name=hsot&id=8&formid=85&amount=30" class="btn btn-primary">$30</a></li>
@@ -42,7 +42,7 @@
 				</a>
 				<h2><a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">Vital Medical Treatments</a></h2>
 				<p class="description">350 donations of $100 provides life-saving surgeries, treatments and vaccinations to our newly rescued pets for one month.</p>
-				<a href="#" class="dontate-btn">[donate]</a>
+				<a target="_blank" href="https://interland3.donorperfect.net/weblink/weblink.aspx?name=hsot&id=8" class="dontate-btn">[donate]</a>
 			</div>
 			<div class="mainContent__item">
 				<a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">
@@ -50,7 +50,7 @@
 				</a>
 				<h2><a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">Make it More Like home</a></h2>
 				<p class="description">It takes 300 donations of $25 to feed and shelter our animals for one day.</p>
-				<a href="#" class="dontate-btn">[donate]</a>
+				<a target="_blank" href="https://interland3.donorperfect.net/weblink/weblink.aspx?name=hsot&id=8" class="dontate-btn">[donate]</a>
 			</div>
 			<div class="mainContent__item">
 				<a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">
@@ -58,7 +58,7 @@
 				</a>
 				<h2><a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">Support the underdogs</a></h2>
 				<p class="description">Often times, our rescues with the biggest hearts and best personalities are overlooked because of their size, age, and fur color.</p>
-				<a href="#" class="dontate-btn">[donate]</a>
+				<a target="_blank" href="https://interland3.donorperfect.net/weblink/weblink.aspx?name=hsot&id=8" class="dontate-btn">[donate]</a>
 			</div>
 			<div class="mainContent__item">
 				<a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">
@@ -66,7 +66,7 @@
 				</a>
 				<h2><a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">Eat. Love. Play.</a></h2>
 				<p class="description">80 donations of $50 will give our rescues bedding, toys, and outdoor play for one month. </p>
-				<a href="#" class="dontate-btn">[donate]</a>
+				<a target="_blank" href="https://interland3.donorperfect.net/weblink/weblink.aspx?name=hsot&id=8" class="dontate-btn">[donate]</a>
 			</div>
 			<div class="mainContent__item">
 				<a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">
@@ -74,7 +74,7 @@
 				</a>
 				<h2><a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">Care for the Community</a></h2>
 				<p class="description">We give free food assistance and low cost veterinary care to people who can't fully provide for their pets, as well as popular community and kids programs.</p>
-				<a href="#" class="dontate-btn">[donate]</a>
+				<a target="_blank" href="https://interland3.donorperfect.net/weblink/weblink.aspx?name=hsot&id=8" class="dontate-btn">[donate]</a>
 			</div>
 			<div class="mainContent__item">
 				<a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">
@@ -82,7 +82,7 @@
 				</a>
 				<h2><a href="https://www.youtube.com/watch?v=9y9oq1U-L9U">Help your pets here</a></h2>
 				<p class="description">Our non-profit animal hospital, which is gold standard (AAHA) certified, gives our community  affordable, top-quality care to homeless animals.</p>
-				<a href="#" class="dontate-btn">[donate]</a>
+				<a target="_blank" href="https://interland3.donorperfect.net/weblink/weblink.aspx?name=hsot&id=8" class="dontate-btn">[donate]</a>
 			</div>
 		</div>
 	</div>
